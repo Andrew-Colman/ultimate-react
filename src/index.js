a09d7bc742e146fb8803bad77b67608bc4e8f7e4
spectralapps <56676505+spectralapps@users.noreply.github.com>
2020-11-02 19:52:53 -0300

    Switch to parcel 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,7 @@
 export default function Home() {
-  const hello = import.meta.env.SNOWPACK_PUBLIC_HELLO_WORLD;
-  const mode = import.meta.env.MODE;
   return (
     <>
-      <div>
-        {hello} {mode}
-      </div>
+      <div>Home</div>
     </>
   );
 }
